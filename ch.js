db91d151af6a588d2c9f382dea50b0c0287d1e6c
Wakko Solis <160749834+solis021022@users.noreply.github.com>
2024-04-22 06:22:43 -0600

    Update ch.js

--- ch.js
+++ ch.js
@@ -1,25 +1,76 @@
-let a =document.querySelector("#a");
-let b = document.querySelector("#b");
-let c = document.querySelector("#c");
-let va =document.querySelector("#va");
-let vb = document.querySelector("#vb");
-let vc = document.querySelector("#vc");
-va.innerHTML = a.value;
-  a.oninput =()=>{
-  va.innerHTML= a.value;
-  let n= parseInt(a.value);
-  }
-  vb.innerHTML = b.value;
-  b.oninput =()=>{
-  vb.innerHTML= b.value;
-  let m= parseInt(b.value);
-  
-  }
-  vc.innerHTML = c.value;
-  c.oninput =()=>{
-  vc.innerHTML= c.value;
-  let m= parseInt(c.value);
-  
-  
-  
+var an= document.querySelector("#an");
+var va= document.querySelector("#va");
+var bn=document.querySelector("#bn");
+var vb= document.querySelector("#vb");
+var cn=document.querySelector("#cn");
+var vc=document.querySelector("#vc");
+var fac=document.querySelector("#fac");
+let ecu=document.querySelector("#ecu");
+
+an.oninput=()=> me();
+bn.oninput=()=> me();
+cn.oninput=()=> me();
+fac.oninput=()=> me();
+ecu.oninput=()=> me()
+const me=()=>{
+    let a=parseFloat(an.value);
+    va.innerHTML=a;
+    let b=parseFloat(bn.value);
+    vb.innerHTML=b;
+    let c= parseFloat(cn.value);
+    vc.innerHTML=c;
+    let R=fac;
+    a2.innerHTML="";
+    a3.innerHTML="";
+    ecu.innerHTML="";
+    var spinner=document.querySelector("#spinner");
+    spinner.innerHTML="<img src='im.gif'>";
+    setTimeout(()=>{
+
+
+let ux=(((-1)*b));
+let ux1=((Math.pow(b,2))-(4*a*c));
+let ux2=(2*a);
+
+let fx1=(ux-Math.sqrt(ux1))/ux2;
+x1.innerHTML=fx1.toFixed(2);
+let sa=(a>=0)?"+"+a:a;
+let sb=(b>=0)?"+"+b:b;
+let sc=(c>=0)?"+"+c:c;
+
+ecu.innerHTML=sa+"x<sup>2</sup>"+sb+"x"+sc+"=0";
+
+
+
+let dx=(ux+Math.sqrt(ux1))/ux2;
+x2.innerHTML=dx.toFixed(2);
+
+if (ux1==0) {
+    R.textContent="Una solucion"
+    let o=ux/ux2;
+    x1.innerHTML="x="+o.toFixed(2);
+    x2.innerHTML="";
+
 }
+else if(ux1>0){
+    R.textContent="Dos soluciones"
+    let dx=(ux+Math.sqrt(ux1))/ux2;
+    let fx1=(ux-Math.sqrt(ux1))/ux2;
+    x1.innerHTML="x1= "+dx.toFixed(2);
+    x2.innerHTML="x2= "+fx1.toFixed(2);
+
+}
+else if(ux1<0){
+    R.textContent="No hay solucion"
+x1.innerHTML=" ";
+x2.innerHTML=" ";
+
+}
+
+
+spinner.innerHTML="";
+
+},1000);
+
+
+      }
