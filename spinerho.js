ba7f5973e8ab151e40f8ea40a59734f7029aeaeb
Wakko Solis <160749834+solis021022@users.noreply.github.com>
2024-04-22 06:42:17 -0600

    Update spinerho.js

--- spinerho.js
+++ spinerho.js
@@ -1,6 +1,6 @@
 var spinner=document.querySelector("#spinner");
 document.querySelector("#btnver").onclick=()=>{
-  spinner.innerHTML="<img src='hola.gif'>";
+  spinner.innerHTML="<img src='hola.gif' width=100px>";
   setTimeout(()=>{
     spinner.innerHTML="<h1>HOLA!!</h1>";
   },1000);
